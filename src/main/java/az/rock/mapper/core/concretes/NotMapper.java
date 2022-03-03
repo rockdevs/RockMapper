1f89c39ed5e7b6379ff5e003c84855ae706713d2
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-03-03 20:05:15 +0400

    added NotMapper.java

---
+++ src/main/java/az/rock/mapper/core/concretes/NotMapper.java
@@ -0,0 +1,11 @@
+package az.rock.mapper.core.concretes;
+
+import az.rock.mapper.core.abstracts.RockMapper;
+
+public class NotMapper<D,T> implements RockMapper<D,T> {
+
+    @Override
+    public D map() {
+        return null;
+    }
+}
